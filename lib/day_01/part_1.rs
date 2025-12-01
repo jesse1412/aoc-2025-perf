bf05b9a7dc443487f1d410d0085c15d9f5ee3fe0
Jesse Serrao <jesseserrao@hotmail.co.uk>
2025-12-01 22:19:23 +0000

    Add day 1

--- lib/day_01/part_1.rs
+++ lib/day_01/part_1.rs
@@ -1,3 +1,17 @@
 pub fn run(input: &str) -> i64 {
-    0
+    let mut tot = 50;
+    let mut zeros = 0;
+    for l in input.lines() {
+        let mul = match &l[0..1] {
+            "R" => 1,
+            "L" => -1,
+            _ => unreachable!(),
+        };
+        let number: i16 = l[1..].parse().expect("should be a number");
+        tot += mul * number;
+        if tot % 100 == 0 {
+            zeros += 1;
+        }
+    }
+    zeros as i64
 }
--- lib/day_01/part_2.rs
+++ lib/day_01/part_2.rs
@@ -1,3 +1,30 @@
 pub fn run(input: &str) -> i64 {
-    0
+    let mut tot = 50_i16;
+    let mut passes = 0;
+    for l in input.lines() {
+        let starting_tot_sign = tot.signum();
+        let mul = match &l[0..1] {
+            "R" => 1,
+            "L" => -1,
+            _ => unreachable!(),
+        };
+        let number: i16 = l[1..].parse().expect("should be a number");
+        let diff = number * mul;
+
+        tot += diff;
+        let pass_flag = match (starting_tot_sign, tot.signum()) {
+            (-1, 1) | (1, -1) => 1,
+            (_, 0) => 1,
+            _ => 0,
+        };
+        passes += (tot / 100).abs() + pass_flag as i16;
+        tot %= 100;
+    }
+    passes as i64
 }
+
+/*
+Need to check for going past 100, 0, or -100
+old num 55 -> new num 205. Gone past 100 and 200, via 205 / 100.
+old num 55 -> new num -101. Gone past -100 and 0, via -101 / 100 (plus 1 for passing 0).
+*/
