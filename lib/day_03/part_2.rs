c3bac3564a02e1b3961c9a464fde20c2168e7b2b
Jesse Serrao <jesseserrao@hotmail.co.uk>
2025-12-03 21:54:59 +0000

    Minor d3p2 clean up + commentary

--- lib/day_03/part_2.rs
+++ lib/day_03/part_2.rs
@@ -2,18 +2,24 @@ pub fn run(input: &str) -> i64 {
     const NUM_INCLUDED: usize = 12;
     let mut tot = 0;
     for l in input.lines() {
-        let mut l_chars = l.chars().enumerate().peekable();
+        // These are the "on batteries".
         let mut digits = ['0'; NUM_INCLUDED];
-        for (c_index, c) in l_chars {
+        // Loop through each char in the line.
+        for (c_index, c) in l.chars().enumerate() {
+            // Loop through each "on battery".
             for digit_index in 0..digits.len() {
                 let digit = digits[digit_index];
+                // If the current char is greater.
                 if c > digit {
                     let remaining_chars = l.len() - (c_index + 1);
                     let remaining_digits = digits.len() - (digit_index + 1);
                     // If there's a number left for every following digit.
                     if remaining_digits <= remaining_chars {
+                        // Take this is the new best value for this battery slot.
                         digits[digit_index] = c;
                         // Zero out every following digit.
+                        // They will get filled by the subsequent chars,
+                        // as the subsequent chars only have to be > 0.
                         for x in digit_index + 1..digits.len() {
                             digits[x] = '0';
                         }
