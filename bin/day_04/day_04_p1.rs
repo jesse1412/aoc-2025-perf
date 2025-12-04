8ccd9207d99f94d887fbbe4ca5a3ff54fe598696
Jesse Serrao <jesseserrao@hotmail.co.uk>
2025-12-04 18:09:08 +0000

    Add day 4

---
+++ bin/day_04/day_04_p1.rs
@@ -0,0 +1,6 @@
+use aoc_2025_perf_lib::day_04::p1;
+
+fn main() {
+    let s = include_str!("../../data/day_04.txt");
+    println!("{}", p1(s));
+}
---
+++ lib/day_04/part_2.rs
@@ -0,0 +1,67 @@
+pub fn run(input: &str) -> i64 {
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|s| s.chars().collect::<Vec<char>>())
+        .collect();
+    let y_limit = grid[0].len() as isize;
+    let x_limit = grid.len() as isize;
+    let mut adjacent_counts: Vec<Vec<u16>> = vec![vec![0; y_limit as usize]; x_limit as usize];
+    let mut roll_positions = Vec::new();
+    for (y, row) in grid.into_iter().enumerate() {
+        for (x, c) in row.into_iter().enumerate() {
+            if c == '@' {
+                roll_positions.push((y, x));
+                add_in_9x9(y_limit, x_limit, &mut adjacent_counts, y, x, true);
+            };
+        }
+    }
+    let mut tot = 0;
+    let mut found_one = true;
+    while found_one {
+        let mut new_roll_positions = Vec::with_capacity(roll_positions.len());
+        found_one = false;
+        for (y, x) in roll_positions {
+            if adjacent_counts[y][x] < 4 {
+                tot += 1;
+                found_one = true;
+                add_in_9x9(y_limit, x_limit, &mut adjacent_counts, y, x, false);
+            } else {
+                new_roll_positions.push((y, x));
+            }
+        }
+        roll_positions = new_roll_positions;
+    }
+    tot
+}
+
+#[inline(always)]
+fn add_in_9x9(
+    y_limit: isize,
+    x_limit: isize,
+    adjacent_counts: &mut [Vec<u16>],
+    y: usize,
+    x: usize,
+    plus: bool,
+) {
+    for y_offset in -1..2 {
+        for x_offset in -1..2 {
+            if x_offset == 0 && y_offset == 0 {
+                continue;
+            }
+            let y = match (y as isize) + y_offset {
+                actual_y if actual_y < y_limit && actual_y >= 0 => actual_y,
+                _ => continue,
+            };
+            let x = match (x as isize) + x_offset {
+                actual_x if actual_x < x_limit && actual_x >= 0 => actual_x,
+                _ => continue,
+            };
+            if plus {
+                adjacent_counts[y as usize][x as usize] += 1;
+            } else {
+                adjacent_counts[y as usize][x as usize] =
+                    adjacent_counts[y as usize][x as usize].saturating_sub(1);
+            }
+        }
+    }
+}
